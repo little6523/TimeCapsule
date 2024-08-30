733b659741aae8df5e6466a050e3ad7de78940f8
grapefriut422 <prikey7@gmail.com>
2024-08-30 17:11:02 +0900

    feat: user DTO 추가

--- src/main/java/com/ahi/timecapsule/dto/UserDTO.java
+++ src/main/java/com/ahi/timecapsule/dto/UserDTO.java
@@ -1,4 +1,32 @@
 package com.ahi.timecapsule.dto;
 
+import com.ahi.timecapsule.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class UserDTO {
+    private String userId;
+    private String password;
+    private String email;
+    private String nickname;
+    private Integer role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static UserDTO fromEntity(User user){
+        return UserDTO.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+
+    }
 }
