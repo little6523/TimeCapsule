2b23ed1767820ff02f03914113e1b1403d23b886
grapefriut422 <prikey7@gmail.com>
2024-08-30 17:29:25 +0900

    refactor: user DTO 컨벤션 적용

--- src/main/java/com/ahi/timecapsule/dto/UserDTO.java
+++ src/main/java/com/ahi/timecapsule/dto/UserDTO.java
@@ -9,24 +9,24 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class UserDTO {
-    private String userId;
-    private String password;
-    private String email;
-    private String nickname;
-    private Integer role;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+  private String userId;
+  private String password;
+  private String email;
+  private String nickname;
+  private Integer role;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
-    public static UserDTO fromEntity(User user){
-        return UserDTO.builder()
-                .userId(user.getUserId())
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-                .password(user.getPassword())
-                .role(user.getRole())
-                .createdAt(user.getCreatedAt())
-                .updatedAt(user.getUpdatedAt())
-                .build();
+  public static UserDTO fromEntity(User user) {
+    return UserDTO.builder()
+            .userId(user.getUserId())
+            .email(user.getEmail())
+            .nickname(user.getNickname())
+            .password(user.getPassword())
+            .role(user.getRole())
+            .createdAt(user.getCreatedAt())
+            .updatedAt(user.getUpdatedAt())
+            .build();
 
-    }
+  }
 }
