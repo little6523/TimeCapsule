aaadc91e000ab427d66e1018a79b5ac6c58dc051
grapefriut422 <prikey7@gmail.com>
2024-08-30 17:03:24 +0900

    feat: user entity 추가

--- src/main/java/com/ahi/timecapsule/entity/User.java
+++ src/main/java/com/ahi/timecapsule/entity/User.java
@@ -1,4 +1,59 @@
 package com.ahi.timecapsule.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
+    @Column(name = "id")
+    private String userId;
+
+    private String password;
+
+    private String email;
+
+    private String nickname;
+
+    private Integer role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Story> stories = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Notice> notices = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public User(String userId, String password, String email, String nickname, Integer role, Integer sharelistId) {
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+
+    }
+
 }
