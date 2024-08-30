2ef1afbe2e44ed1e10ae47e46307104417d876c9
grapefriut422 <prikey7@gmail.com>
2024-08-30 17:30:17 +0900

    feat: user repository 추가

--- src/main/java/com/ahi/timecapsule/repository/UserRepository.java
+++ src/main/java/com/ahi/timecapsule/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package com.ahi.timecapsule.repository;
 
-public interface UserRepository {
+import com.ahi.timecapsule.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+  Optional<User> findByUserId(String id);
+  Optional<User> findByEmail(String email);
+  Optional<User> findByNickname(String nickname);
+
+  boolean existsByUserId(final String id);
+  boolean existsByEmail(final String email);
+  boolean existsByNickname(final String nickname);
 }
