aab614b6bd8f03159c3a13f9d8112e0c9f0b0abe
yelinee <ami573@naver.com>
2024-09-01 00:02:14 +0900

    feat: Story repository 추가

--- src/main/java/com/ahi/timecapsule/repository/StoryRepository.java
+++ src/main/java/com/ahi/timecapsule/repository/StoryRepository.java
@@ -1,4 +1,12 @@
 package com.ahi.timecapsule.repository;
 
-public interface StoryRepository {
+import com.ahi.timecapsule.entity.Story;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryRepository extends JpaRepository<Story, Integer> {
+  Page<Story> findByUser_Id(String id, Pageable pageable);
+
+  Page<Story> findByStoryShares_User_Id(String id, Pageable pageable);
 }
