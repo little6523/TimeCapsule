201ea951e17cd9385aa63831102e6f69cb0f9b47
SooMin <pepsi_soomae@naver.com>
2024-09-02 23:51:15 +0900

    Comment: Notice Repository 주석 수정
    제목과 내용 -> 제목 또는 내용

--- src/main/java/com/ahi/timecapsule/repository/NoticeRepository.java
+++ src/main/java/com/ahi/timecapsule/repository/NoticeRepository.java
@@ -14,7 +14,7 @@ public interface NoticeRepository extends JpaRepository<Notice, Integer> {
   // 최신 날짜 기준 게시글 나오는 메서드
   Page<Notice> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-  // 제목과 내용 검색 후 최신 날짜 기준으로 나오는 메서드
+  // 제목 또는 내용 검색 후 최신 날짜 기준으로 나오는 메서드
   @Query("SELECT n FROM Notice n WHERE n.title LIKE %:title% OR n.content LIKE %:content% ORDER BY n.createdAt DESC")
   Page<Notice> findByTitleOrContentOrderByCreatedAtDesc(@Param("title") String title, @Param("content") String content, Pageable pageable);
 }
