452e779aa76384a2ea30d414f278a2f64277afe8
grapefriut422 <prikey7@gmail.com>
2024-09-02 17:54:08 +0900

    feat: 회원가입 관련 DTO, UserService, UserController 추가

--- src/main/java/com/ahi/timecapsule/controller/UserController.java
+++ src/main/java/com/ahi/timecapsule/controller/UserController.java
@@ -1,4 +1,112 @@
 package com.ahi.timecapsule.controller;
 
+import com.ahi.timecapsule.dto.UserDTO;
+import com.ahi.timecapsule.dto.UserLoginDTO;
+import com.ahi.timecapsule.dto.UserSignUpDTO;
+import com.ahi.timecapsule.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping
 public class UserController {
+  private final UserService userService;
+
+  @GetMapping("/signUpPage")
+  public String signUpPage(Model model) {
+    UserDTO userSignUp = UserDTO.builder().build();
+    model.addAttribute("userSignUp", userSignUp);
+    return "signup";
+  }
+
+  @PostMapping("/signUp")
+  public String signUp(@Valid @ModelAttribute("userSignUp") UserSignUpDTO userSignUp, BindingResult bindingResult, Model model) {
+
+    // 비밀번호 일치 여부 확인
+    if (!userService.isPasswordMatching(userSignUp)) {
+      bindingResult.rejectValue("confirmPassword", "error.userDto", "비밀번호가 일치하지 않습니다.");
+    }
+    // 백엔드 단에서도 유효성 검증
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("errorMessage", "입력값에 오류가 있습니다. 다시 확인해주세요.");
+      return "signup";
+    }
+
+    try {
+      userService.registerUser(userSignUp);
+    } catch (DataIntegrityViolationException e) {  // 데이터베이스 제약 조건 위반 예외 처리 (아이디, 이메일, 닉네임 중복)
+      model.addAttribute("errorMessage", e.getMessage());
+      return "signup";  // 예외 발생 시 회원가입 폼으로 다시 이동
+    } catch (Exception e) {  // 그 외의 일반적인 예외 처리
+      model.addAttribute("errorMessage", "알 수 없는 오류가 발생했습니다.");
+      return "signup";
+    }
+
+    return "redirect:/login";
+  }
+
+  @GetMapping("/login")
+  public String login(Model model) {
+    UserDTO userLogin = UserDTO.builder().build();
+    model.addAttribute("userLogin", userLogin);
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String login(@ModelAttribute("userLogin") UserLoginDTO userLogin, HttpServletResponse response, Model model) {
+    System.out.println("시작!");
+    System.out.println(userLogin.getUserId());
+    String token = userService.login(userLogin);
+    System.out.println("토큰생성");
+
+    if (token != null) {
+      Cookie jwtCookie = new Cookie("jwt", token);
+      jwtCookie.setPath("/");
+      jwtCookie.setMaxAge(3600);
+      jwtCookie.setHttpOnly(true); // 자바스크립트에서 접근 불가하게 설정
+//            jwtCookie.setSecure(true);
+      response.addCookie(jwtCookie);
+      System.out.println("꺄악");
+
+      return "redirect:/main";
+    } else {
+      model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+      return "login";
+    }
+  }
+
+  // 메인 페이지로 이동
+  @GetMapping("/main")
+  public String mainPage() {
+    return "main";
+  }
+
+  // 아이디, 이메일, 닉네임 중복 확인
+  @PostMapping("/api/users/check-duplicate")
+  public ResponseEntity<Boolean> checkDuplicate(@RequestBody Map<String, String> request) {
+    String field = request.get("field");
+    String value = request.get("value");
+    boolean isDuplicate = userService.isDuplicate(field, value);
+    return ResponseEntity.ok(isDuplicate);
+  }
+
+  // 비밀번호 매칭 확인
+  @PostMapping("/api/users/password-match")
+  public ResponseEntity<Boolean> isPasswordMatching(@RequestBody UserSignUpDTO userSignUp){
+    boolean isMatch = userService.isPasswordMatching(userSignUp);
+    return ResponseEntity.ok(isMatch);
+  }
+
 }
--- src/main/java/com/ahi/timecapsule/service/UserService.java
+++ src/main/java/com/ahi/timecapsule/service/UserService.java
@@ -1,4 +1,106 @@
 package com.ahi.timecapsule.service;
 
+import com.ahi.timecapsule.config.JwtTokenProvider;
+import com.ahi.timecapsule.config.PasswordEncoderConfig;
+import com.ahi.timecapsule.dto.UserLoginDTO;
+import com.ahi.timecapsule.entity.User;
+import com.ahi.timecapsule.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ahi.timecapsule.dto.UserDTO;
+import com.ahi.timecapsule.dto.UserSignUpDTO;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoderConfig passwordencoderconfig;
+  private final AuthenticationManager authenticationManager;
+  private final JwtTokenProvider jwtTokenProvider;
+
+  private final Integer role_user = 1;
+
+  @Transactional
+  public UserDTO registerUser(UserSignUpDTO usersignupdto) {
+    // 중복 검사
+    if (existsByUserId(usersignupdto.getUserId())) {
+      throw new DataIntegrityViolationException("이미 존재하는 사용자 ID입니다.");
+    }
+    if (existsByEmail(usersignupdto.getEmail())) {
+      throw new DataIntegrityViolationException("이미 존재하는 이메일입니다.");
+    }
+    if (existsByNickname(usersignupdto.getNickname())) {
+      throw new DataIntegrityViolationException("이미 존재하는 닉네임입니다.");
+    }
+    // 비밀번호 암호화
+    String password = passwordencoderconfig.passwordEncoder().encode(usersignupdto.getPassword());
+
+    //User 객체로 빌딩
+    User user = User.builder()
+            .userId(usersignupdto.getUserId())
+            .email(usersignupdto.getEmail())
+            .nickname(usersignupdto.getNickname())
+            .password(password)
+            .role(role_user)
+            .build();
+
+    return UserDTO.fromEntity(userRepository.save(user));
+  }
+
+  @Transactional
+  public Boolean isPasswordMatching(UserSignUpDTO usersignupdto) {
+    return usersignupdto.getPassword().equals(usersignupdto.getConfirmPassword());
+  }
+
+  @Transactional
+  public String login(UserLoginDTO userlogindto) {
+    System.out.println("service옴");
+    Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(userlogindto.getUserId(), userlogindto.getPassword()));
+    System.out.println("1");
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    System.out.println("여기까지");
+    System.out.println(jwtTokenProvider.generateToken(authentication));
+    return jwtTokenProvider.generateToken(authentication);
+
+
+  }
+
+  @Transactional
+  public boolean isDuplicate(String field, String value) {
+    switch (field) {
+      case "userId":
+        return existsByUserId(value);
+      case "email":
+        return existsByEmail(value);
+      case "nickname":
+        return existsByNickname(value);
+      default:
+        throw new IllegalArgumentException("잘못된 필드 이름: " + field);
+    }
+  }
+
+  @Transactional
+  public boolean existsByUserId(String userId) {
+    return userRepository.existsById(userId);
+  }
+
+  @Transactional
+  public boolean existsByEmail(String email) {
+    return userRepository.existsByEmail(email);
+  }
+
+  @Transactional
+  public boolean existsByNickname(String nickname) {
+    return userRepository.existsByNickname(nickname);
+  }
+
 }
