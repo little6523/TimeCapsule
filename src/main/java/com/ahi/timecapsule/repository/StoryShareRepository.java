3e0bfed39e603a0dd44e16967d9abfe20735d1bf
yelinee <ami573@naver.com>
2024-09-04 14:12:51 +0900

    feat: image, storyshare, user repository 추가

---
+++ src/main/java/com/ahi/timecapsule/repository/StoryShareRepository.java
@@ -0,0 +1,7 @@
+package com.ahi.timecapsule.repository;
+
+import com.ahi.timecapsule.entity.StoryShare;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryShareRepository extends JpaRepository<StoryShare, Integer> {
+}
