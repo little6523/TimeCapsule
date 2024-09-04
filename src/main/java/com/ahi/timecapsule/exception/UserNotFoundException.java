b76d9c2137ca00054270c272a7ef8487d50b8c9d
SooMin <pepsi_soomae@naver.com>
2024-09-04 09:57:00 +0900

    style : Notice 예외처리 코드 재정리

--- src/main/java/com/ahi/timecapsule/exception/UserNotFoundException.java
+++ src/main/java/com/ahi/timecapsule/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.ahi.timecapsule.exception;
 public class UserNotFoundException extends RuntimeException {
 
   public UserNotFoundException(String userId) {
-    super("사용자 아이디를 찾을 수 없습니다." +  userId );
+    super("사용자 아이디를 찾을 수 없습니다." + userId);
   }
 }
