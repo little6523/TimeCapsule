8326e4cbdca221ab21ab7fabe1f8447d9b2957cc
hyeonjun.lee <little6523@naver.com>
2024-09-10 10:18:24 +0900

    feat: 스토리 공유자 기능 추가
    #29

---
+++ src/main/java/com/ahi/timecapsule/repository/StoryShareRepository.java
@@ -0,0 +1,8 @@
+package com.ahi.timecapsule.repository;
+
+import com.ahi.timecapsule.entity.StoryShare;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryShareRepository extends JpaRepository<StoryShare, Long> {
+
+}
