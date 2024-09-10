e012f3b7e8ab5ece3f523ad327c0705a7d4a0e13
hyeonjun.lee <little6523@naver.com>
2024-09-10 10:22:49 +0900

    feat: Image 엔티티 추가

---
+++ src/main/java/com/ahi/timecapsule/entity/Image.java
@@ -0,0 +1,23 @@
+package com.ahi.timecapsule.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "images")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Image {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "story_id", nullable = false)
+  private Story story;
+
+  @Column private String url;
+}
