c4e472532386e4e5167edc1af97193b8dbb2cb30
hyeonjun.lee <little6523@naver.com>
2024-09-10 10:21:52 +0900

    feat: 스토리 레포지토리 추가

--- src/main/java/com/ahi/timecapsule/repository/StoryRepository.java
+++ src/main/java/com/ahi/timecapsule/repository/StoryRepository.java
@@ -1,4 +1,9 @@
 package com.ahi.timecapsule.repository;
 
-public interface StoryRepository {
+import com.ahi.timecapsule.entity.Story;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryRepository extends JpaRepository<Story, Long> {
+
+
 }
